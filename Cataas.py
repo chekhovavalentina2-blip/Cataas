27eab0ba95d47eefd3c064b5380df166ea667446
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-24 15:29:08 +0300

    ++

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url = "https://cataas.com/cat"
+ing = load_image(url)
+
+if ing:
+    label.config(image=ing)
+    label.image = ing
+
+window.mainloop()
